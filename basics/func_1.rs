96812ddaf9d6073f8f8398ca6c73f8f8029285be
pcdinh <pcdinh@gmail.com>
2013-01-06 15:02:08 +0700

    Some basic functions

--- basics/func_1.rs
+++ basics/func_1.rs
@@ -1,6 +1,9 @@
 fn test_branching(value: ~str) -> ~str {
     // https://github.com/mozilla/rust/issues/3463
     // https://github.com/mozilla/rust/issues/3294
+	// https://mail.mozilla.org/pipermail/rust-dev/2012-August/002231.html Ignore trailing semicolons
+	// https://github.com/mozilla/rust/issues/1712 Expression syntax and semicolon
+	// https://github.com/mozilla/rust/blob/master/src/test/run-pass/utf8.rs
     return if value == ~"hello_world" {
                ~"Hello World"
            } else if value == ~"" {
